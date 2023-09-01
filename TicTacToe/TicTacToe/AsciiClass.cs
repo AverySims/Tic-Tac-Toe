855f6fb4cd84da95adaa13d9f318af3ea40cab10
Avery <avery.j.sims@gmail.com>
2023-09-01 15:00:13 -0500

    Added new functions to AsciiClass, randomized input for game in program

--- TicTacToe/TicTacToe/AsciiClass.cs
+++ TicTacToe/TicTacToe/AsciiClass.cs
@@ -2,13 +2,43 @@
 
 public class AsciiClass
 {
-    public void PrintAsciiBoard(char[][] c)
-    {
-        string board = $" {c[0][0]} | {c[0][1]} | {c[0][2]} \n" +
-                       "---+---+---\n" +
-                       $" {c[1][0]} | {c[1][1]} | {c[1][2]} \n" +
-                       "---+---+---\n" +
-                       $" {c[2][0]} | {c[2][1]} | {c[2][2]} \n";
-        Console.WriteLine(board);
-    }
+	public void PrintAsciiBoard(char[,] n)
+	{
+		string a = $" {n[0,0]} | {n[0,1]} | {n[0,2]} ";
+		string b = $" {n[1,0]} | {n[1,1]} | {n[1,2]} ";
+		string c = $" {n[2,0]} | {n[2,1]} | {n[2,2]} ";
+		string d = "---+---+---";
+
+		string[] board = {a, d, b, d, c};
+
+		foreach (var row in board)
+		{
+			Console.WriteLine(row);
+		}
+	}
+
+	public void ResetBoard(ref char[,] n)
+	{
+		// Reset the board to empty spaces
+		for (int row = 0; row < n.GetLength(0); row++)
+		{
+			for (int col = 0; col < n.GetLength(1); col++)
+			{
+				n[row, col] = ' ';
+			}
+		}
+	}
+
+	public void RandomizeBoard(ref char[,] n)
+	{
+		// Randomize the board to X's and O's
+		Random random = new Random();
+		for (int row = 0; row < n.GetLength(0); row++)
+		{
+			for (int col = 0; col < n.GetLength(1); col++)
+			{
+				n[row, col] = random.Next(0, 6) >= 3 ? 'X' : 'O';
+			}
+		}
+	}
 }
--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,22 +1,33 @@
-﻿namespace TicTacToe
+﻿namespace TicTacToe;
+
+internal class Program
 {
-	internal class Program
+	public char PlayerChar { get; private set; } = ' ';
+	public char OpponentChar { get; private set; } = 'O';
+
+	private static char[,] currentBoard = new char[3,3];
+
+	static bool _loopMain = true;
+	static void	Main(string[] args)
 	{
-		public char Player { get; private set; } = ' ';
-		public char Opponent { get; private set; } = 'O';
-		
-		static void	Main(string[] args)
+		AsciiClass ascii = new AsciiClass();
+
+		while (_loopMain)
 		{
-			AsciiClass ascii = new AsciiClass();
-			
-			char[] a = {'X', ' ', ' ' };
-			char[] b = {' ', 'O', ' ' };
-			char[] c = {' ', ' ', 'X' };
-			char[][] board = {a, b, c};
-
-			ascii.PrintAsciiBoard(board);
-
-			Console.ReadLine();
+			// Reset the board to empty spaces, then randomize it
+			ascii.ResetBoard(ref currentBoard);
+			ascii.RandomizeBoard(ref currentBoard);
+			ascii.PrintAsciiBoard(currentBoard);
+	
+			Console.WriteLine("Use your number pad to play.");
+			if (int.TryParse(Console.ReadLine(), out int input))
+			{
+				Console.Clear();
+			}
+			else
+			{
+				Console.Clear();
+			}
 		}
 	}
 }
